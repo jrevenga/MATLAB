8249726568421bf9cc0fbb0a054f76cb7c64c3e2
Jrevenga06 <jorge.revenga@edu.uah.es>
2023-12-11 11:45:58 +0100

    .

--- Practica2/code/parte1/prueba.m
+++ Practica2/code/parte1/prueba.m
@@ -1,14 +1,21 @@
 %Tiempo de muestreo
 Ts=100e-3;
 % Referencia x-y de posicion
-x_0 = 0;
-y_0 = 0.15;
+x_0 = 0.1;
+y_0 = 0.1;
+th_0 = (3*pi)/180;
+
 % Ejecutar Simulacion
 sim('PositionControl.slx')
+
 % Mostrar
 x=salida_x.signals.values;
 y=salida_y.signals.values;
+x2=entrada_x.signals.values;
+y2=entrada_y.signals.values;
+
 figure;
-plot(x,y);
+plot(x,y,x2,y2);
+legend('Borroso','Generado')
 grid on;
 hold on;
