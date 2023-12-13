c020f4799c4997c8ba03d77c0e7c2ead57ca59a6
Jrevenga06 <jorge.revenga@edu.uah.es>
2023-12-13 06:18:22 +0100

    .

--- Practica2/code/parte1/simulacion2.m
+++ Practica2/code/parte1/simulacion2.m
@@ -1,19 +1,25 @@
-%Tiempo de muestreo
-Ts=100e-3;
+% Tiempo de muestreo
+Ts = 100e-3;
+
 % Referencia x-y de posicion
 x_0 = 0.1;
-y_0 = 0.2;
+y_0 = 0;
 th_0 = 0;
 
-% Ejecutar Simulacion
-sim('PositionControl2.slx')
+% Ejecutar simulación
+sim('PositionControl2.slx');
 
-% Mostrar
+% Obtener datos de salida
 x=salida_x.signals.values;
 y=salida_y.signals.values;
-
+    
+% Visualizar la trayectoria y el objeto
 figure;
-plot(x,y);
-legend('Trayectoria')
+plot(x, y);
 grid on;
 hold on;
+    
+title('Simulación');
+legend('Trayectoria');
+xlabel('Posición X');
+ylabel('Posición Y');
