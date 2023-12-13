714248445f380fa1b94c0cd380efbcdb7a5ec8ee
Jrevenga06 <jorge.revenga@edu.uah.es>
2023-12-13 05:34:16 +0100

    .

--- Practica2/code/parte1/simulacion.m
+++ Practica2/code/parte1/simulacion.m
@@ -1,27 +1,34 @@
 % Tiempo de muestreo
 Ts = 100e-3;
 
-% Referencia x-y de posición aleatoria
-refx = 5;
-refy = -5;
+% Número de simulaciones a realizar
+numSimulations = 5;
 
-% Ejecutar simulación
-sim('PositionControl.slx');
+for i = 1:numSimulations
+    % Referencia x-y de posición aleatoria
+    refx = 15 * rand - 5;
+    refy = 15 * rand - 5;
 
-% Obtener datos de salida
-x = salida_x.signals.values;
-y = salida_y.signals.values;
-    
-% Visualizar la trayectoria y el objeto
-figure;
-plot(x, y);
-grid on;
-hold on;
-    
-% Mostrar referencia
-plot(refx, refy, 'ro', 'MarkerSize', 10, 'MarkerFaceColor', 'r');
-    
-title('Simulación');
-legend('Trayectoria', 'Referencia');
-xlabel('Posición X');
-ylabel('Posición Y');
+    % Ejecutar simulación
+    sim('PositionControl.slx');
+
+    % Obtener datos de salida
+    x = salida_x.signals.values;
+    y = salida_y.signals.values;
+
+    % Visualizar la trayectoria
+    figure;
+    plot(x, y);
+    grid on;
+    hold on;
+
+    % Mostrar referencia
+    plot(refx, refy, 'ro', 'MarkerSize', 10, 'MarkerFaceColor', 'r');
+    title(['Simulación ', num2str(i)]);
+    legend('Trayectoria', 'Referencia');
+    xlabel('Posición X');
+    ylabel('Posición Y');
+
+    % Pausa para visualizar antes de pasar a la siguiente simulación
+    pause(1);
+end
